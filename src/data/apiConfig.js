8c01a3f8e3df51fcb35404e050c87b4a3823d817
Aman Kumar Singh <er.aks473@gmail.com>
2023-08-12 13:44:38 +0530

    GoogleLogni fix

--- src/data/apiConfig.js
+++ src/data/apiConfig.js
@@ -1,5 +1,6 @@
-export const hostServer = "http://192.168.1.6:8080";
-export const google_client_id = "";
+export const hostServer = "http://192.168.1.5:8080";
+export const google_client_id =
+  "963062247454-m7fo18guqi4t6e4lr8tdvvgcb8i5ldt7.apps.googleusercontent.com";
 
 export const getAuthHeader = (user) => {
   return {
